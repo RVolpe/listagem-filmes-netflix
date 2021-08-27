26d133201f32626b91bac24c47a02bb6cb2a6841
Rodrigo Volpe <kild_ro@hotmail.com>
2021-08-27 11:30:11 -0300

    Primeiro commit

---
+++ src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -0,0 +1,78 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { debounceTime } from 'rxjs/operators';
+import { FilmesService } from 'src/app/core/filmes.service';
+import { ConfigParams } from 'src/app/shared/models/config-params';
+import { Filme } from 'src/app/shared/models/filme';
+
+@Component({
+  selector: 'dio-listagem-filmes',
+  templateUrl: './listagem-filmes.component.html',
+  styleUrls: ['./listagem-filmes.component.scss']
+})
+export class ListagemFilmesComponent implements OnInit {
+  readonly semFoto = 'https://www.termoparts.com.br/wp-content/uploads/2017/10/no-image.jpg';
+
+  config: ConfigParams = {
+    pagina: 0,
+    limite: 4
+  };
+
+  filmes: Filme[] = [];    // array vazia
+  filtrosListagem: FormGroup;
+  generos: Array<string>;
+
+  constructor(private filmesService: FilmesService,
+              private fb: FormBuilder,
+              private router: Router) { }
+
+  ngOnInit(): void {
+    this.filtrosListagem = this.fb.group({
+      texto: [''],
+      genero: ['']
+    });
+
+    // pesquisa texto
+    this.filtrosListagem.get('texto').valueChanges
+      .pipe(debounceTime(400))
+      .subscribe((val: string) => {
+        this.config.pesquisa = val;
+        this.resetarConsulta();
+    });
+
+    // pesquisa genero
+    this.filtrosListagem.get('genero').valueChanges
+      .subscribe((val: string) => {
+        this.config.campo = {tipo: 'genero', valor: val};
+        this.resetarConsulta();
+    });
+
+
+    this.generos = ["Ação", "Aventura", "Ficção Científica", "Romance", "Terror", "Comédia", "Drama", "Fantasia"];
+
+    this.listarFilmes();
+  }
+
+  onScroll(): void {
+    this.listarFilmes();
+  }
+
+  abrir(id: number): void {
+    this.router.navigateByUrl('/filmes/' + id);
+  }
+
+  private listarFilmes(): void {
+    this.config.pagina++;
+    this.filmesService.listar(this.config)
+      .subscribe((filmesAux: Filme[]) => this.filmes.push(...filmesAux));
+  }
+
+  // inicializa as variaveis
+  private resetarConsulta(): void {
+    this.config.pagina = 0;
+    this.filmes = [];
+    this.listarFilmes();
+  }
+
+}
---
+++ src/app/shared/components/campos/validar-campos.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ValidarCamposService {
+
+  constructor() { }
+
+  hasErrorValidar(control: AbstractControl, errorName: string): boolean {
+    if ((control.touched || control.dirty) && this.hasError(control, errorName)) {
+      return true;
+    }
+    return false;
+  }
+
+  private hasError(control: AbstractControl, errorName: string): boolean {
+    return control.hasError(errorName);
+  }
+
+  lengthValidar(control: AbstractControl, errorName: string): number {
+    const error = control.errors[errorName];
+    return error.requiredLength || error.min || error.max || 0;
+  }
+}
